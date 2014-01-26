57e37bd81efecbd678d22ae1bf59188171afa9a3
BossGrand <Boss@CharlesHaro.com>
2014-01-26 14:27:08 -0800

    doors have sound now

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -13,25 +13,32 @@ public class Door : MonoBehaviour {
 
     }
 
+    bool opened = false;
     public void open()
     {
-        Invoke("close", 3f);
-        gameObject.GetComponent<BoxCollider2D>().enabled = false;
-        foreach(Transform t in transform)
+        if (!opened)
         {
+            Invoke("close", 1.5f);
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            SoundManager.Instance.PlaySound(SoundManager.Instance.Clips[0]);
+            foreach (Transform t in transform)
+            {
 
-            if(t.tag == "OpenDoor")
-                t.gameObject.SetActive(true);
-            if (t.tag == "ClosedDoor")
-                t.gameObject.SetActive(false);
+                if (t.tag == "OpenDoor")
+                    t.gameObject.SetActive(true);
+                if (t.tag == "ClosedDoor")
+                    t.gameObject.SetActive(false);
+            }
+            opened = true;
         }
-
-
     }
 
     private void close()
     {
         gameObject.GetComponent<BoxCollider2D>().enabled = true;
+        SoundManager.Instance.PlaySound(SoundManager.Instance.Clips[1]);
+        CancelInvoke("close");
+        opened = false;
         foreach (Transform t in transform)
         {
             if (t.tag == "OpenDoor")
