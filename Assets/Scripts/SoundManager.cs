3f5ba5a700cd1ba659d066dda16905344595d1c4
BossGrand <Boss@CharlesHaro.com>
2014-01-26 11:35:56 -0800

    Sound Manger is set

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = GameObject.FindObjectOfType(typeof(SoundManager)) as SoundManager;
+            return instance;
+        }
+
+    }
+    private static SoundManager instance;
+
+    void Awake()
+    {
+        audio.Play();
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        Debug.Log("playing sound" + clip.name);
+        audio.PlayOneShot(clip);
+    }
+}
+
