4fb27408987758c077288a4b762895781522f9da
BossGrand <Boss@CharlesHaro.com>
2014-01-26 15:10:31 -0800

    wincondition added

--- Assets/Scripts/WinCondition.cs
+++ Assets/Scripts/WinCondition.cs
@@ -12,4 +12,24 @@ public class WinCondition : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+
+        if (collider.tag == "Player")
+        {
+            Player p = collider.gameObject.GetComponent<Player>();
+            if (p.GetType() == typeof(Survivor))
+            {
+                GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>().EscapePlayer(p);
+            }
+
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        
+        }
+    }
 }
